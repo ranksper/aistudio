979a432517c1b4c4786dce1645501977eb7a9548
ranksper <ranksper@gmail.com>
2024-08-15 18:29:13 +0530

    setup google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import { Metadata } from "next";
 import { Providers } from "@/app/providers";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 import "@/styles/globals.css";
 
@@ -23,6 +24,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link rel="apple-touch-icon" sizes="180x180" href={"/assets/favicons/apple-touch-icon.png"} />
                 <link rel="icon" type="image/png" sizes="32x32" href={"/assets/favicons/favicon-32x32.png"} />
                 <link rel="icon" type="image/png" sizes="16x16" href={"/assets/favicons/favicon-16x16.png"} />
+
+                <Script id="google-tagmanager" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-JFSFMGCFKM" />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-JFSFMGCFKM');
+                    `}
+                </Script>
                 <link rel="manifest" href={"/assets/favicons/site.webmanifest"} />
             </head>
             <body className={`h-screen bg-white dark:bg-slate-900 ${inter.className}`}>
